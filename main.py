65ca54eb66d370112889c84fc9cdec6ad8dfff19
absolutxpp <dmzmarcus@gmail.com>
2026-02-10 08:40:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 from pyscript import document
-import time
 import random
+import time
 
 
 def loops_sample(e):
 
     result = document.getElementById("result")
+    junk = []
 
     while True:
 
-        for i in range(5000):
+        big = ""
+        for i in range(3000):
+            big += f"Generating Robux {random.randint(1,999999)} 💰<br>"
 
-            result.innerHTML += f"Generating Robux {random.randint(1,999999)} 💰<br>"
+        result.innerHTML = big
+
+
+        for x in range(40000):
+            junk.append(x * x * x)
+
+        if len(junk) > 200000:
+            junk.clear()
 
         time.sleep(0.01)
-        
-        waste = []
-        for x in range(10000):
-            waste.append(x * x * x)
