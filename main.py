07d1f51e3a626c2fb57495184870512932cc8efa
absolutxpp <dmzmarcus@gmail.com>
2026-02-10 08:38:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-from pyscript import display, document
+from pyscript import document
+import time
+import random
 
 
 def loops_sample(e):
-    document.getElementById('result').innerHTML = " "
 
-while True:
-    print("Generating Robux") #endless
+    result = document.getElementById("result")
+
+    while True:
+
+        for i in range(5000):
+
+            result.innerHTML += f"Generating Robux {random.randint(1,999999)} 💰<br>"
+
+        time.sleep(0.01)
+        
+        waste = []
+        for x in range(10000):
+            waste.append(x * x * x)
